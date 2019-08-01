6e42865de6d946be829ea34fbac2d33f1ea7dac6
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-01 18:11:48 +0300

    1.6.6
    server info

--- bot.js
+++ bot.js
@@ -54,6 +54,19 @@ bot.on("message", async message => {
         return;
     }
 
+    if( command === `${prefix}serverinfo`){
+        let server_embed = new Discord.RichEmbed()
+        .setTitle("***Server Info***")
+        .setThumbnail(message.guild.iconURL)
+        .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+        .addField("**Server Name**", `${message.guild.name}`)
+        .addField("**Server Members**", `${message.guild.memberCount}`)
+        .addField("**Server Join**", `You joined ${message.member.joinedAt}`)
+        .addField("**Server Birth**", `This server was created ${message.guild.createdAt}`)
+        .setFooter("Cute | Bot", bot.user.displayAvatarURL);
+        message.channel.send(server_embed);
+    }
+
     return;
 })
 
