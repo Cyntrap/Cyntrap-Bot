fb389c1fc1c144c73c6337a1fc263db77774ca2d
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-01 21:05:34 +0300

    1.8.12
    updated join command

--- commands/join.js
+++ commands/join.js
@@ -2,17 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(message.member.voiceChannel){
-        if(!message.member.voiceConnection){
-            message.member.voiceChannel.join()
-            .then(connection =>{
-                message.reply("Successfully joined!")
-            })
-        }
-    }else{
-        message.channel.send("You must be in a voice channel!");
-    }
+    const voiceChannel = message.member.voiceChannel;
 
+    if(voiceChannel){
+        voiceChannel.join();
+    }else return message.channel.send("You must be in a voice channel!");
 }
 
 module.exports.help = {
