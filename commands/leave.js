12ba3e0130e23f9a8300d3e8b6ca8967169acbbb
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-01 19:51:14 +0300

    1.8.2
    3131

--- commands/leave.js
+++ commands/leave.js
@@ -3,10 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     if(message.guild.voiceConnection){
-        message.guild.voiceChannel.disconnect();
-    }
-    else{
-        message.channel.send("I must be in a voice channel!");
+        message.guild.voiceConnection.disconnect();
     }
 
 }
