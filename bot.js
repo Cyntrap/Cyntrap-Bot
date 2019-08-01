eec92639d2c4fa4b0415fc3a1a55af2640765d2b
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-01 18:41:13 +0300

    1.7.3
    fixing command handler

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ bot.on("message", async message => {
     }
 
     let messageArray = message.content.split(" ");
-    let command = messageArray[0];
+    let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
