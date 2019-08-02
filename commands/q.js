22a9535359f87bfcbe3d81d7f9f71e4e9fcd42db
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-02 13:56:00 +0300

    1.9.28
    big queue update

--- commands/q.js
+++ commands/q.js
@@ -11,12 +11,22 @@ ${serverQueue.songs.map(song => `*${song.title}`).join('\n')}
 **Now Playing** ${serverQueue.songs[0].title}
     `)
     **/
+   let length = serverQueue.songs.length - 9
     let queue_embed = new Discord.RichEmbed()
     .setTitle("🎵 ***Song Queue*** 🎵")
     .setDescription(`
-${serverQueue.songs.map(song => `*${song.title}`).join('\n')}
+    1 -> ${serverQueue.songs[0].title}
+    2 -> ${serverQueue.songs[1].title}
+    3 -> ${serverQueue.songs[2].title}
+    4 -> ${serverQueue.songs[3].title}
+    5 -> ${serverQueue.songs[4].title}
+    6 -> ${serverQueue.songs[5].title}
+    7 -> ${serverQueue.songs[6].title}
+    8 -> ${serverQueue.songs[7].title}
+    9 -> ${serverQueue.songs[8].title}
+    10 -> ${serverQueue.songs[9].title}
 
-**Now Playing** ${serverQueue.songs[0].title}
+    There is ${length} more songs..
     `)
     .setFooter("Cute Bot", bot.user.displayAvatarURL);
 
