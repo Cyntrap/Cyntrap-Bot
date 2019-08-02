c58a49983d705430fff66a4b222f33fd3104aeef
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-03 02:39:41 +0300

    1.10.25

--- commands/skip.js
+++ commands/skip.js
@@ -8,8 +8,8 @@ module.exports = class skip {
     }
 
     async run (bot, message, args, serverQueue){
-		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
-		if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
+		if (!message.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
+		if (!serverQueue) return message.channel.send('There is nothing playing that I could skip for you.');
 		serverQueue.connection.dispatcher.end('Skip command has been used!');
 		return undefined;
     }
