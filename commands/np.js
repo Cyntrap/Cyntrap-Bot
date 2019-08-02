548aa5a9260619b77a0cd93bdb4b7a618abb3331
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-02 12:40:08 +0300

    1.9.18
    now playing command

---
+++ commands/np.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, serverQueue, queue) => {
+
+    if(!serverQueue.songs[0]) return message.channel.send("No song is currrenly playing");
+
+    serverQueue.textChannel.send(`***Now Playing*** -- ${queueConstruct.songs[0].title}`);
+
+}
