c968704784be1dd5730a9b95716eb38927aa220f
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-02 13:45:41 +0300

    1.9.26
    queue visual update

--- commands/q.js
+++ commands/q.js
@@ -3,13 +3,23 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, serverQueue, queue) =>{
 
     if(!serverQueue) return message.channel.send(" No Queue >_< ")
-
+/**
     message.channel.send(`
     🎵 ***Song Queue*** 🎵
-    ${serverQueue.songs.map(song => `*${song.title}`).join('\n')}
+${serverQueue.songs.map(song => `*${song.title}`).join('\n')}
 
-    **Now Playing** ${serverQueue.songs[0].title}
+**Now Playing** ${serverQueue.songs[0].title}
     `)
+    **/
+    let queue_embed = new Discord.RichEmbed()
+    .setTitle("🎵 ***Song Queue*** 🎵")
+    .setDescription(`
+${serverQueue.songs.map(song => `*${song.title}`).join('\n')}
+
+**Now Playing** ${serverQueue.songs[0].title}
+    `)
+    .setFooter("Cute Bot", bot.user.displayAvatarURL);
+
 
 
 }
