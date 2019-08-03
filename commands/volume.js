9074ea7c0d42f37447ecaebb67d07e8b33722e05
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-03 12:47:51 +0300

    1.10.39

--- commands/volume.js
+++ commands/volume.js
@@ -10,11 +10,11 @@ module.exports = class volume {
     async run (bot, message, args, serverQueue){
         if(!message.member.voiceChannel) return message.channel.send("You are not in a voice channel. Dummy dum");
         if(!serverQueue) return message.channel.send("There is no queue");
-        message.channel.send(`Current volume: ${serverQueue.volume}`);
-        serverQueue.volume = args[1];
-        if(serverQueue.volume > 10){
+        if(args[1] > 10){
             return message.channel.send("Volume can't be higher than 10")
         }else{
+            serverQueue.volume = args[1];
+            message.channel.send(`Current Volume: ${serverQueue.volume}`);
             serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 5);
         }
         
