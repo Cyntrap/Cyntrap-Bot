b317b496fcb5ffaf14ae1a8b353c7bbd1505178b
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-03 11:55:27 +0300

    1.10.29
    playlist update

--- commands/play.js
+++ commands/play.js
@@ -23,49 +23,72 @@ module.exports = class play {
 			return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
 		}
 
-		try {
-			var video = await youtube.getVideo(url);
-		} catch (error) {
-			try {
-				var videos = await youtube.searchVideos(searchString, 1);
-				var video = await youtube.getVideoByID(videos[0].id);
-			} catch (err) {
-				console.error(err);
-				return message.channel.send('I could not obtain any search results.');
+
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)){
+			const playlist = await youtube.getPlaylist(url);
+			const videos = await playlist.getVideos();
+
+			for(const video of Object.values(videos)){
+				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
 			}
-		}
-		console.log(video);
-		const song = {
-			id: video.id,
-			title: video.title,
-			url: `https://www.youtube.com/watch?v=${video.id}`
-		};
-		if (!serverQueue) {
-			const queueConstruct = {
-				textChannel: message.channel,
-				voiceChannel: voiceChannel,
-				connection: null,
-				songs: [],
-				volume: 5,
-				playing: true
-			};
-			queue.set(message.guild.id, queueConstruct);
-
-			queueConstruct.songs.push(song);
-
+			return message.channel.send(`Playlist: **${playlist.title}** has been added to the queue`);
+		}else{
 			try {
-				var connection = await voiceChannel.join();
-				queueConstruct.connection = connection;
-				play(message.guild, queueConstruct.songs[0]);
+				var video = await youtube.getVideo(url);
 			} catch (error) {
-				console.error(`I could not join the voice channel: ${error}`);
-				queue.delete(message.guild.id);
-				return message.channel.send(`I could not join the voice channel: ${error}`);
+				try {
+					var videos = await youtube.searchVideos(searchString, 1);
+					var video = await youtube.getVideoByID(videos[0].id);
+				} catch (err) {
+					console.error(err);
+					return message.channel.send('I could not obtain any search results.');
+				}
 			}
-		} else {
-			serverQueue.songs.push(song);
-			console.log(serverQueue.songs);
-			return message.channel.send(`**${song.title}** has been added to the queue!`);
+			return handleVideo(video, message, voiceChannel);
+		}
+
+
+		async function handleVideo(video, message, voiceChannel, playlits = false){
+
+			const serverQueue = queue.get(message.guild.id);
+			const song = {
+				id: video.id,
+				title: video.title,
+				url: `https://www.youtube.com/watch?v=${video.id}`
+			};
+			if (!serverQueue) {
+				const queueConstruct = {
+					textChannel: message.channel,
+					voiceChannel: voiceChannel,
+					connection: null,
+					songs: [],
+					volume: 5,
+					playing: true
+				};
+				queue.set(message.guild.id, queueConstruct);
+	
+				queueConstruct.songs.push(song);
+	
+				try {
+					var connection = await voiceChannel.join();
+					queueConstruct.connection = connection;
+					play(message.guild, queueConstruct.songs[0]);
+				} catch (error) {
+					console.error(`I could not join the voice channel: ${error}`);
+					queue.delete(message.guild.id);
+					return message.channel.send(`I could not join the voice channel: ${error}`);
+				}
+			} else {
+				serverQueue.songs.push(song);
+				console.log(serverQueue.songs);
+				if(playlist){
+					return undefined;
+				}else{
+					return message.channel.send(`**${song.title}** has been added to the queue!`);
+				}
+			}
+			return undefined;
 		}
 
 		function play(guild, song) {
