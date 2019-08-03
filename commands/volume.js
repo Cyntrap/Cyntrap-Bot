2ef499b4e7ce7370402fdc9f7246c5e967193908
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-03 12:36:16 +0300

    1.10.37
    volume limit

--- commands/volume.js
+++ commands/volume.js
@@ -11,7 +11,7 @@ module.exports = class volume {
         if(!message.member.voiceChannel) return message.channel.send("You are not in a voice channel. Dummy dum");
         if(!serverQueue) return message.channel.send("There is no queue");
         message.channel.send(`Current volume: ${serverQueue.volume}`);
-        if(parseInt(args[1]) > 1000){
+        if(parseInt(args[1]) > 100){
             return message.channel.send("The volume cannot be set to more than a 1000");
         }
         serverQueue.volume = args[1];
