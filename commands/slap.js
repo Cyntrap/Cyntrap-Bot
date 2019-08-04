a0147908dfb7c5512330aa715fa1171319ced77b
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-04 17:30:56 +0300

    1.12.2

---
+++ commands/slap.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+
+module.exports = class slap {
+    constructor(){
+        this.name = "slap"
+        this.alias = "hit"
+        this.usage = "_slap"
+    }
+
+    async run (bot, message, args){
+
+
+        const gifs = [
+            "https://media1.tenor.com/images/1cf84bf514d2abd2810588caf7d9fd08/tenor.gif?itemid=7679403",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ]
+
+
+        let victim = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+        let user = message.author;
+        let slap_embed = new Discord.RichEmbed()
+        .setColor("PURPLE")
+        .setTitle(`${user.tag} hit ${victim.user.tag}`)
+        .setImage("https://media1.tenor.com/images/1cf84bf514d2abd2810588caf7d9fd08/tenor.gif?itemid=7679403")
+        message.channel.send(slap_embed);
+    }
+}
