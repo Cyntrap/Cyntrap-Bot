56fb790fa8e3430c1382198c94e354fa8d861c64
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-04 14:33:57 +0300

    1.11.2
    bug fix

--- commands/clear.js
+++ commands/clear.js
@@ -10,9 +10,9 @@ module.exports = class clear{
 
     async run (bot, message, args){
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permission to do that!");
-        if(!args[0]) return message.channel.send("Please provide a number!");
-        message.channel.bulkDelete(args[0]).then( () => {
-            message.channel.send(`Cleard ${args[0]} messages.`).then(msg => msg.delete(5000));
+        if(!args[1]) return message.channel.send("Please provide a number!");
+        message.channel.bulkDelete(args[1]).then(() => {
+            message.channel.send(`Cleard ${args[1]} messages.`).then(msg => msg.delete(5000));
         });
     }
 
