6efb323b973f8cb5b9833b7547c5225fad400458
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-04 17:58:07 +0300

    1.12.8

--- commands/slap.js
+++ commands/slap.js
@@ -20,15 +20,16 @@ module.exports = class slap {
             "https://thumbs.gfycat.com/LegalExhaustedEuropeanpolecat-size_restricted.gif",
             "https://media1.tenor.com/images/3fd96f4dcba48de453f2ab3acd657b53/tenor.gif?itemid=14358509",
             "https://media1.tenor.com/images/31686440e805309d34e94219e4bedac1/tenor.gif?itemid=4874411",
+            "https://cdn.weeb.sh/images/ryn_Zg5JG.gif"
         ]
 
         let index = Math.floor((Math.random() * gifs.length));
         let victim = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-        let user = message.member;
+        let user = message.author;
         let slap_embed = new Discord.RichEmbed()
         .setColor("PURPLE")
-        .setTitle(`${user} hit ${victim}`)
         .setImage(gifs[index]);
+        message.channel.send(`**${user}** hit **${victim}**`)
         message.channel.send(slap_embed);
     }
 }
