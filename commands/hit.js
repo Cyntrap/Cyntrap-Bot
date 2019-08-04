abd8c244fda3b17336fa2172e4f7356dc7f81093
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-04 17:24:03 +0300

    1.12
    Fun update woo
    adding hit

---
+++ commands/hit.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = class hit {
+    constructor(){
+        this.name = "hit"
+        this.alias = "punch"
+        this.usage = "_hit"
+    }
+
+    async run (bot, message, args){
+
+        let victim = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+        let user = message.author;
+        let hit_embed = Discord.RichEmbed()
+        .setColor("PURPLE")
+        .setTitle(`${user.tag} hit ${victim.tag}`)
+        .setImage("https://3.bp.blogspot.com/-bnRsbQGOMaY/V3O84p3t5lI/AAAAAAAAHvw/yQJsPll1erQ8cF14oR3nF3sJip2FELyGQCKgB/s640/bobbing-head.gif")
+        .setFooter("Cute Bot", bot.user.displayAvatarURL);
+        message.channel.send(hit_embed);
+    }
+}
