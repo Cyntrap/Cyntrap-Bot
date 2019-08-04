f84ca8f8fa18935e6881c1fef980b02824b7e0f7
Cyntrap <41926961+Cyntrap@users.noreply.github.com>
2019-08-04 17:41:39 +0300

    1.12.4
    slap updates

--- commands/slap.js
+++ commands/slap.js
@@ -13,23 +13,23 @@ module.exports = class slap {
         const gifs = [
             "https://media1.tenor.com/images/1cf84bf514d2abd2810588caf7d9fd08/tenor.gif?itemid=7679403",
             "https://media1.tenor.com/images/4eed54377433c396ce2d9ad9ee5d22ef/tenor.gif?itemid=11234788",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "https://media1.giphy.com/media/tMIWyF5GUrWwM/source.gif",
+            "https://pa1.narvii.com/6807/ac91cef2e5ae98f598665193f37bba223301d75c_hq.gif",
+            "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
+            "https://i.imgur.com/oOCq3Bt.gif",
+            "https://media1.tenor.com/images/f619012e2ec268d73ecfb89af5a8fb51/tenor.gif?itemid=8562186",
+            "https://thumbs.gfycat.com/LegalExhaustedEuropeanpolecat-size_restricted.gif",
+            "https://media1.tenor.com/images/3fd96f4dcba48de453f2ab3acd657b53/tenor.gif?itemid=14358509",
+            "https://media1.tenor.com/images/31686440e805309d34e94219e4bedac1/tenor.gif?itemid=4874411",
         ]
 
-
+        let index = Math.floor((Math.random() * gifs.length));
         let victim = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
         let user = message.author;
         let slap_embed = new Discord.RichEmbed()
         .setColor("PURPLE")
-        .setTitle(`@${user.tag} hit @${victim.user.tag}`)
-        .setImage("https://media1.tenor.com/images/1cf84bf514d2abd2810588caf7d9fd08/tenor.gif?itemid=7679403")
+        .setImage(gifs[index]);
+        message.channel.send(`@${user.tag} hit @${victim.user.tag}`);
         message.channel.send(slap_embed);
     }
 }
